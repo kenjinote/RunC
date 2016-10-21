88a3312e6d97a75db4ed1332aa0b33e20fd7ec60
kenjinote <kenjinote@DESKTOP-MUMGVTF>
2016-10-21 18:36:18 +0900

    add icon.

--- Source.cpp
+++ Source.cpp
@@ -387,7 +387,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 		0,
 		DLGWINDOWEXTRA,
 		hInstance,
-		0,
+		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)),
 		LoadCursor(0,IDC_SIZEWE),
 		0,
 		0,
